619d29b612ccc5329b9444c91391eda7582e5062
Glitch (strong-synonymous-hornet) <none>
2021-09-08 14:47:59 +0000

    🙎🏹 Checkpoint ./controllers/station.js:35422279/213 ./views/partials/addstation.hbs:35422279/297 ./views/station.hbs:35422279/14 ./routes.js:35422279/92 ./controllers/dashboard.js:35422279/345 ./models/station-store.js:35422279/140

--- controllers/station.js
+++ controllers/station.js
@@ -2,6 +2,7 @@
 
 const logger = require('../utils/logger');
 const stationStore = require('../models/station-store.js');
+const uuid = require('uuid');
 
 const station = {
   index(request, response) {
@@ -18,18 +19,20 @@ const station = {
     const stationId = request.params.id;
     const station = stationStore.getStation(stationId);
     const newReading = {
+      id: uuid.v1(),
       code: request.body.code,
       temperature: request.body.temperature,
       windSpeed: request.body.windSpeed,
       pressure: request.body.pressure,
     };
+    logger.debug('New Reading = ', newReading);
     stationStore.addReading(stationId, newReading);
     response.redirect('/station/' + stationId);
   },
   
   deleteReading(request, response) {
     const stationId = request.params.id;
-    const readingId = request.params.readingId;
+    const readingId = request.params.readingid;
     logger.debug(`Deleting Reading ${readingId} from Station ${stationId}`);
     stationStore.removeReading(stationId, readingId);
     response.redirect('/station/' + stationId);
